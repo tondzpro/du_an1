a558bd59e9acde28d2a605a83837bd5498802c01
Nguyen Tien Dat <secretangent2112@gmail.com>
2022-11-21 18:36:09 +0700

    D2

---
+++ Layout/footer.php
@@ -0,0 +1,73 @@
+ <!-- footer chung -->
+ <div class="Footer">
+        <div class="col-lg-3" style="float: left; font-size: 16px;">
+            <h4 class="footer-title">HỆ THỐNG CỬA HÀNG</h4>
+            <a href="#" class="ft-content">27 Chùa Bộc, Đống Đa, HN</a> <br>
+            <a href="#" class="ft-content">242 Thái Hà, Đống Đa, HN</a> <br>
+            <a href="#" class="ft-content">63 Đại Cổ Việt, Hai Bà Trưng, HN</a> <br>
+            <a href="#" class="ft-content">20 Dương Quảng Hàm, Cầu Giấy, HN</a> <br>
+            <a href="#" class="ft-content">346 Cầu Giấy, Cầu Giấy, HN</a> <br>
+            <a href="#" class="ft-content">116 Hồ Tùng Mậu, Cầu Giấy, HN</a>
+        </div>
+
+        <div class="col-lg-3" style="float: left; font-size: 16px;">
+            <H4 class="footer-title">HỖ TRỢ KHÁCH HÀNG</H4>
+            <a href="#" class="ft-content">Hướng Dẫn Mua Hàng</a> <br>
+            <a href="#" class="ft-content">Hình Thức Thanh Toán</a> <br>
+            <a href="#" class="ft-content">Quy Định Về Bảo Mật Thông Tin</a> <br>
+            <a href="#" class="ft-content">Chính Sách Bảo Hành</a> <br>
+            <a href="#" class="ft-content">Chính Sách Đổi Hàng</a> <br>
+            <a href="#" class="ft-content">Chính Sách Vận Chuyển</a> <br>
+            <a href="#" class="ft-content">Điều khoản dịch vụ</a> <br>
+            <a href="#" class="ft-content">Giới thiệu</a> <br>
+        </div>
+
+        <div class="col-lg-3" style="float: left; font-size: 16px;">
+            <div class="register">
+                <h4 class="footer-title">THEO DÕI CHÚNG TÔI</h4>
+                <input type="email" placeholder="Nhập email"
+                    style=" height: 30px; font-size: smaller; border-radius: 3px;"> <button
+                    type="submit"
+                    style="background-color:rgb(69, 68, 68); color: white; font-size: smaller; border-radius: 3px; height: 30px;">Đăng
+                    ký</button>
+            </div> <br> <br>
+            <div class="social">
+                <h5>KẾT NỐI VỚI CHÚNG TÔI TẠI</h5>
+                <a href="#" class="iconft"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="iconft"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="iconft"><i class="fab fa-pinterest"></i></a>
+                <a href="#" class="iconft"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="iconft"> <i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3" style="float: left;">
+        <br>
+        <p><b>Tư vấn mua hàng miễn phí:</b></p>
+        <p style="color: red;"><b>1800 1234 (Nhánh 1)</b></p>
+        <p><b>Hỗ trợ kỹ thuật:</b></p>
+        <p style="color: red;"><b>1800 1234 (Nhánh 2)</b></p>
+
+        <p><b>Hỗ trợ thanh toán:</b></p>
+        <p>
+            <i class="fab fa-cc-visa fa-2xl"></i>
+            <i class="fab fa-cc-mastercard fa-2xl"></i>
+            <i class="fab fa-apple-pay fa-2xl"></i>
+            <i class="fab fa-google-pay fa-2xl"></i>
+        </p>
+    </div>
+    <!--Optional JavaScript-->
+    <!--jQuery first, then Popper.js, then Bootstrap JS-->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jspivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
